3a956949c18612e130ae5c46e2fc8cf70a1c5031
Lee Seung Jin <dltmdwls154@gmail.com>
2024-02-25 21:28:59 +0900

    first Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import CustomTable from './components/CustomDataTable.jsx';
+// import CustomTable from './components/CustomDataTable.jsx';
 
 function App() {
   return (
     <div className={'min-h-[100vh] w-full bg-[gray]'}>
-      <CustomTable />
+      <div>sdfasdfaaasssaaadfff</div>
     </div>
   );
 }
